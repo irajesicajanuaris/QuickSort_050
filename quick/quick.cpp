f3ecbdec6255aaa460fdc1cc82ff5f67afc2c356
irajesicajanuaris <jjiraaa1@gmail.com>
2023-03-30 10:59:59 +0700

    membuat fungsi input

--- quick/quick.cpp
+++ quick/quick.cpp
@@ -6,3 +6,26 @@ int arr[20];
 int cmp_cout = 0; // number of comparasion
 int mov_count = 0; // number of sata movements
 int n;
+
+void  input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMkasimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
